7cd7cab9f217dccff75031a28e8f3fde37191371
artgalla8 <artgalla8@gmail.com>
2022-04-17 21:30:09 +0100

    working on js

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ canvas = document.getElementsById("confetti");
 context = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+conx = context.canvas.width/2;
+cony = context.canvas.height/2;
+
+let confetti = [];
+const confettiCount = 300;
+//
