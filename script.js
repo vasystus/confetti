0922380ff8c9e74c51980e01a26b327898c563ac
artgalla8 <artgalla8@gmail.com>
2022-04-19 21:00:29 +0100

    working on js/2

--- script.js
+++ script.js
@@ -7,4 +7,18 @@ cony = context.canvas.height/2;
 
 let confetti = [];
 const confettiCount = 300;
-//
+const gravity = 0.5;
+const velocity = 5;
+const drag = 0.075;
+const colors = [
+    {front: 'red', back: 'darkred'},
+    {front: 'orange', back: 'darkorange'},
+    {front: 'yellow', back: 'darkyellow'},
+    {front: 'green', back: 'darkgreen'},
+    {front: 'turquoise', back: 'darkturqoise'},
+    {front: 'blue', back: 'darkblue'},
+    {front: 'purple', back: 'darkpurple'},
+    {front: 'pink', back: 'darkpink'}
+];
+
+
